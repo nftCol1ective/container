d17d9ca3b8f063f16499952e1af131a68dcd43b0
Michael Vogt <info@michaelvogt.eu>
2022-03-05 20:00:24 +0100

    initial commit of template front end

---
+++ app/.eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
+  env: {
+    es6: true,
+    browser: true
+  },
+  plugins: [
+    'svelte3'
+  ],
+  overrides: [
+    {
+      files: ['*.svelte'],
+      processor: 'svelte3/svelte3'
+    }
+  ],
+  rules: {
+    'no-empty': 'off',
+    'no-empty-function': 'off',
+  },
+};
