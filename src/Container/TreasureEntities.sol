f12c476e49b3343809690bea7f550bf9ccba31d0
Airesh Bhat <nelliairesh@gmail.com>
2022-03-09 23:03:10 +0530

    Add struct to keep a record of entities (#3)
    * Add struct to keep a record of entities

--- src/Container/TreasureEntities.sol
+++ src/Container/TreasureEntities.sol
@@ -14,12 +14,35 @@ contract TreasureEntities is ERC1155PresetMinterPauser, Ownable {
     uint256 public constant SILVER = 1;
     uint256 public constant ELIXER = 2;
 
+    struct Entities {
+        uint256 sizeOfMapping;
+        mapping(uint256 => string) entityNames;
+    }
+
+    Entities public entityList;
+
+    function addValue(string memory entityName) private onlyOwner {
+        entityList.sizeOfMapping++;
+        entityList.entityNames[entityList.sizeOfMapping] = entityName;
+    }
+
     mapping(uint256 => string) private _uris;
 
     constructor(string memory uri) public ERC1155PresetMinterPauser(uri) {
+        addValue("GOLD");
         _mint(msg.sender, GOLD, 10**18, "");
+        addValue("SILVER");
         _mint(msg.sender, SILVER, 10**27, "");
+        addValue("ELIXER");
         _mint(msg.sender, ELIXER, 10**18, "");
+    }
+
+    function getEntitiesAvailable() public view returns (string[] memory) {
+        string[] memory entitiesArray = new string[](entityList.sizeOfMapping);
+        for (uint256 i = 0; i < entityList.sizeOfMapping; i++) {
+            entitiesArray[i] = entityList.entityNames[i + 1];
+        }
+        return entitiesArray;
     }
 
     function uri(uint256 tokenId) public view override returns (string memory) {
