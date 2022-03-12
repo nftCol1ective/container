211085c169d05aad2460bacae03b0d2bca74d000
AIRESH BHAT <nelliairesh@gmail.com>
2022-03-12 14:46:54 +0530

    Fix stack overflow bug

--- src/Container/LoogieTank.sol
+++ src/Container/LoogieTank.sol
@@ -8,13 +8,11 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-import 'base64-sol/base64.sol';
+import "base64-sol/base64.sol";
 import "hardhat/console.sol";
 import "./TreasureEntities.sol";
 
-
 contract LoogieTank is ERC721Enumerable, Ownable, ERC1155Holder {
-
     using Strings for uint256;
     using Strings for uint8;
     using Counters for Counters.Counter;
@@ -24,13 +22,13 @@ contract LoogieTank is ERC721Enumerable, Ownable, ERC1155Holder {
     bytes4 private constant INTERFACE_ERC721 = 0x80ac58cd;
 
     // uint256 constant public price = 5000000000000000; // 0.005 eth
-    uint256 constant public price = 0; // 0.005 eth
+    uint256 public constant price = 0; // 0.005 eth
 
     struct Entity {
         uint256 blockAdded;
         uint256 id;
         address addr;
-        uint8 tokenType;    // ERC721 (0) or 1155 (1)
+        uint8 tokenType; // ERC721 (0) or 1155 (1)
         uint8 x;
         uint8 y;
         uint8 scale;
@@ -38,23 +36,14 @@ contract LoogieTank is ERC721Enumerable, Ownable, ERC1155Holder {
         int8 dy;
     }
 
-    string[] containerTypes = [
-        "Gaming",
-        "Membership",
-        "Art",
-        "Ticket",
-        "Ownership",
-        "Avatar",
-        "Collection"
-    ];
+    string[] containerTypes = ["Gaming", "Membership", "Art", "Ticket", "Ownership", "Avatar", "Collection"];
 
-    mapping( uint256 => Entity[]) public EntitiesByTankId;
-    mapping( address => uint256[]) public tanksByOwner;
-    mapping( uint256 => address) public itemsetByTankId;
+    mapping(uint256 => Entity[]) public EntitiesByTankId;
+    mapping(address => uint256[]) public tanksByOwner;
+    mapping(uint256 => address) public itemsetByTankId;
 
     event ContainerMinted(uint256 id);
     event EntityReceived(uint256 id, uint256 value);
-
 
     constructor() ERC721("Tank", "TANK") {}
 
@@ -66,13 +55,17 @@ contract LoogieTank is ERC721Enumerable, Ownable, ERC1155Holder {
         itemsetByTankId[tankId] = _entitiesContractAddress;
     }
 
-    function setupNewContainer(uint256 containerType, uint256[] memory items, uint256[] memory amount) public returns (uint256) {
+    function setupNewContainer(
+        uint256 containerType,
+        uint256[] memory items,
+        uint256[] memory amount
+    ) public returns (uint256) {
         uint256 id = mintItem();
 
-//         TreasureEntities itemTokens = new TreasureEntities('');
-//         itemTokens.setApprovalForAll(msg.sender, true);
+        //         TreasureEntities itemTokens = new TreasureEntities('');
+        //         itemTokens.setApprovalForAll(msg.sender, true);
 
-        for(uint256 index = 0; index < items.length; index++) {
+        for (uint256 index = 0; index < items.length; index++) {
             // itemTokens.mint(address(this), items[index], amount[index], bytes(abi.encodePacked(id)));
         }
 
@@ -94,7 +87,7 @@ contract LoogieTank is ERC721Enumerable, Ownable, ERC1155Holder {
         return id;
     }
 
-    function ownerTankIds() external view returns (uint256[] memory)  {
+    function ownerTankIds() external view returns (uint256[] memory) {
         return tanksByOwner[msg.sender];
     }
 
@@ -102,7 +95,7 @@ contract LoogieTank is ERC721Enumerable, Ownable, ERC1155Holder {
         uint256 tankCount = tanksByOwner[msg.sender].length;
 
         string[] memory result = new string[](tankCount);
-        for(uint256 index = 0; index < tankCount; index++) {
+        for (uint256 index = 0; index < tankCount; index++) {
             result[index] = tokenURI(tanksByOwner[msg.sender][index]);
         }
 
@@ -123,43 +116,57 @@ contract LoogieTank is ERC721Enumerable, Ownable, ERC1155Holder {
     function tokenURI(uint256 id) public view override returns (string memory) {
         require(_exists(id), "token does not exist");
 
-        string memory _name = string(abi.encodePacked('Loogie Tank #', id.toString()));
-        string memory description = string(abi.encodePacked('Loogie Tank'));
+        string memory _name = string(abi.encodePacked("Loogie Tank #", id.toString()));
+        string memory description = string(abi.encodePacked("Loogie Tank"));
         string memory image = Base64.encode(bytes(generateSVGofTokenById(id)));
 
-        return string(abi.encodePacked(
-                'data:application/json;base64,',
-                Base64.encode(
-                    bytes(
-                        abi.encodePacked(
-                            '{"',
-                                'name":"', _name, '",',
-                                '"description":"', description, '",',
-                                '"image": "data:image/svg+xml;base64,', image, '",',
-                                '"tokenid":"', Strings.toString(id),
-                            '"}'
+        return
+            string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"',
+                                'name":"',
+                                _name,
+                                '",',
+                                '"description":"',
+                                description,
+                                '",',
+                                '"image": "data:image/svg+xml;base64,',
+                                image,
+                                '",',
+                                '"tokenid":"',
+                                Strings.toString(id),
+                                '"}'
+                            )
                         )
                     )
                 )
-            ));
+            );
     }
 
     function generateSVGofTokenById(uint256 id) internal view returns (string memory) {
-        string memory svg = string(abi.encodePacked(
+        string memory svg = string(
+            abi.encodePacked(
                 '<svg width="310" height="310" xmlns="http://www.w3.org/2000/svg">',
                 renderTokenById(id),
-                '</svg>'
-            ));
+                "</svg>"
+            )
+        );
 
         return svg;
     }
 
     // Visibility is `public` to enable it being called by other contracts for composition.
     function renderTokenById(uint256 id) public view returns (string memory) {
-        string memory render = string(abi.encodePacked(
+        string memory render = string(
+            abi.encodePacked(
                 '<rect x="0" y="0" width="310" height="310" stroke="black" fill="#8FB9EB" stroke-width="5"/>',
                 renderComponent(id)
-            ));
+            )
+        );
         return render;
     }
 
@@ -175,70 +182,91 @@ contract LoogieTank is ERC721Enumerable, Ownable, ERC1155Holder {
             newX = newPos(c.dx, blocksTravelled, c.x);
             newY = newPos(c.dy, blocksTravelled, c.y);
 
-            svg = string(abi.encodePacked(
+            svg = string(
+                abi.encodePacked(
                     svg,
-                    '<g>',
+                    "<g>",
                     '<animateTransform attributeName="transform" dur="1500s" fill="freeze" type="translate" additive="sum" ',
-                    'values="', newX.toString(), ' ', newY.toString(), ';'));
+                    'values="',
+                    newX.toString(),
+                    " ",
+                    newY.toString(),
+                    ";"
+                )
+            );
 
             for (uint8 j = 0; j < 100; j++) {
                 newX = newPos(c.dx, 1, newX);
                 newY = newPos(c.dy, 1, newY);
 
-                svg = string(abi.encodePacked(
-                        svg,
-                        newX.toString(), ' ', newY.toString(), ';'));
+                svg = string(abi.encodePacked(svg, newX.toString(), " ", newY.toString(), ";"));
             }
 
             uint8 scale = c.scale;
             string memory scaleString = "";
             if (scale != 0) {
-                scaleString = string(abi.encodePacked('values="0.', scale.toString(), ' 0.', scale.toString(), '"'));
+                scaleString = string(abi.encodePacked('values="0.', scale.toString(), " 0.", scale.toString(), '"'));
             }
 
             string memory _svg;
 
             try TreasureEntities(itemsetByTankId[_id]).renderTokenById(0) returns (string memory __svg) {
                 _svg = __svg;
-            } catch {return "";}
+            } catch {
+                return "";
+            }
 
-            svg = string(abi.encodePacked(
+            svg = string(
+                abi.encodePacked(
                     svg,
                     '"/>',
-                    '<animateTransform attributeName="transform" type="scale" additive="sum" ', scaleString, '/>',
+                    '<animateTransform attributeName="transform" type="scale" additive="sum" ',
+                    scaleString,
+                    "/>",
                     _svg,
-                    '</g>'));
+                    "</g>"
+                )
+            );
         }
 
         return svg;
     }
 
-    function newPos(int8 speed, uint8 blocksTraveled, uint8 initPos) internal pure returns (uint8) {
+    function newPos(
+        int8 speed,
+        uint8 blocksTraveled,
+        uint8 initPos
+    ) internal pure returns (uint8) {
         uint8 traveled;
         uint8 start;
 
         if (speed >= 0) {
-        unchecked {
-            traveled = blocksTraveled * uint8(speed);
-            start = initPos + traveled;
-        }
+            unchecked {
+                traveled = blocksTraveled * uint8(speed);
+                start = initPos + traveled;
+            }
             return start;
         } else {
-        unchecked {
-            traveled = blocksTraveled * uint8(- speed);
-            start = initPos - traveled;
-        }
+            unchecked {
+                traveled = blocksTraveled * uint8(-speed);
+                start = initPos - traveled;
+            }
             return start;
         }
     }
 
     function sendEthToOwner() external {
-        (bool success,) = owner().call{value : address(this).balance}("");
+        (bool success, ) = owner().call{value: address(this).balance}("");
         require(success, "could not send ether");
     }
 
     // Receive new NFT
-    function transferNFT(address nftAddr, uint256 tokenId, uint256 tankId, uint8 scale) external {
+    function transferNFT(
+        address nftAddr,
+        uint256 tokenId,
+        uint256 tankId,
+        uint8 scale
+    ) external {
         require(ERC721(nftAddr).ownerOf(tokenId) == msg.sender, "you need to own the NFT");
         require(ownerOf(tankId) == msg.sender, "you need to own the tank");
         require(nftAddr != address(this), "nice try!");
@@ -250,23 +278,53 @@ contract LoogieTank is ERC721Enumerable, Ownable, ERC1155Holder {
         registerToken(nftAddr, tankId, tokenId, 1, 0);
     }
 
-    function onERC1155Received(address operator, address from, uint256 id, uint256 value, bytes memory data) public override returns (bytes4) {
-        console.log('received token with id ', id);
+    function onERC1155Received(
+        address operator,
+        address from,
+        uint256 id,
+        uint256 value,
+        bytes memory data
+    ) public override returns (bytes4) {
+        console.log("received token with id ", id);
 
-        registerToken(from, abi.decode(data, (uint)), id, value, 1);
+        registerToken(from, abi.decode(data, (uint256)), id, value, 1);
         return super.onERC1155Received(operator, from, id, value, data);
     }
 
-    function registerToken(address from, uint tankId, uint256 tokenId, uint256 value, uint8 tokenType) internal {
-        bytes32 randish = keccak256(abi.encodePacked(blockhash(block.number - 1), msg.sender, address(this), tokenId, tankId));
-        EntitiesByTankId[tankId].push(Entity(block.number, tokenId, from, tokenType,
-            uint8(randish[0]), uint8(randish[1]), 1, int8(uint8(randish[2])), int8(uint8(randish[3]))));
+    function registerToken(
+        address from,
+        uint256 tankId,
+        uint256 tokenId,
+        uint256 value,
+        uint8 tokenType
+    ) internal {
+        bytes32 randish = keccak256(
+            abi.encodePacked(blockhash(block.number - 1), msg.sender, address(this), tokenId, tankId)
+        );
+        EntitiesByTankId[tankId].push(
+            Entity(
+                block.number,
+                tokenId,
+                from,
+                tokenType,
+                uint8(randish[0]),
+                uint8(randish[1]),
+                1,
+                int8(uint8(randish[2])),
+                int8(uint8(randish[3]))
+            )
+        );
 
         emit EntityReceived(tokenId, value);
     }
 
     // Allows to extend both ERC721 and ERC1155Holder contracts from OpenZeppelin
-    function supportsInterface(bytes4 interfaceId) public view override(ERC721Enumerable, ERC1155Receiver) returns (bool) {
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721Enumerable, ERC1155Receiver)
+        returns (bool)
+    {
         return super.supportsInterface(interfaceId);
     }
 }
